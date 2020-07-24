1fa1e5c4447e9c9f0eac90bc7d9bb3730bd1fb42
Mark Bradshaw <notthehoople@googlemail.com>
2020-07-24 22:25:55 +0100

    day01 part b complete - refactored part a so method SumNumbersWithJump could be reused

---
+++ day01/day01/Numbers.cs
@@ -0,0 +1,37 @@
+﻿namespace day01
+{
+    public class Numbers
+    {
+        private string numberString { get; set; }
+
+        public Numbers(string inputString)
+        {
+            numberString = inputString;
+        }
+
+        public int SumNumbers()
+        {
+            return SumNumbersWithJump(1);
+        }
+
+        // Now, instead of considering the next digit, it wants you to consider the digit halfway around the circular list.
+        // That is, if your list contains 10 items, only include a digit in your sum if the digit 10/2 = 5 steps forward matches it.
+        // Fortunately, your list has an even number of elements.
+        public int SumNumbersWithJump(int jumpAheadBy = 0)
+        {
+            var runningTotal = 0;
+
+            if (jumpAheadBy == 0)
+                jumpAheadBy = numberString.Length / 2;
+
+            for (var pos = 0; pos < numberString.Length; pos++)
+            {
+                var jumpAheadPos = (pos + jumpAheadBy) % numberString.Length;
+                if (numberString[pos] == numberString[jumpAheadPos])
+                    runningTotal += (int)numberString[pos] - '0';
+            }
+
+            return runningTotal;
+        }
+    }
+}
